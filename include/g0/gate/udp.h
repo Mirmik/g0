df7e7a4a27bd67fdacd6dd8e3bddf35d7b7993b2
mirmik <mirmikns@yandex.ru>
2018-02-26 21:00:23 +0300

    a

--- include/g0/gate/udp.h
+++ include/g0/gate/udp.h
@@ -10,20 +10,24 @@ namespace g0 {
 		gxx::hostaddr addr;
 		uint16_t port;
 		bool temporary;
+		id_t node;
 		udp_gate_address(gxx::hostaddr& str, int port, bool temporary) : addr(str), port(port), temporary(temporary) {}
 	};
 
 	class udp_gate : public g0::service {
 	private:
+		int idcounter = 0;
+
 		gxx::inet::udp_socket sock;
-		std::list<udp_gate_address> table;
+		std::unordered_map<std::pair<gxx::inet::hostaddr, int>, udp_gate_address*> addrmap; 
+		std::unordered_map<id_t, udp_gate_address*> idmap;
 
 	public:	
 		udp_gate() {}
 		udp_gate(unsigned int port) : sock("0.0.0.0", port) {}
 		void init(unsigned int port) { sock.bind("0.0.0.0", port); }
 
-		void add(gxx::hostaddr addr, int port) { table.emplace_back(addr, port, false); }
+		void add(gxx::hostaddr addr, int port) { }
 		void on_input(g0::message*) override;
 		void read_handler();
 		int get_fd() { return sock.fd; }
